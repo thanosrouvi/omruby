d8ca8b9e749688cbb29b4e656ccdd7518b2d7d09
“Antonita <adonita2211@homtail.com>
2023-02-12 15:12:48 +0200

    facebook log in_3

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'login'=>'pages#login'
   get 'signUp'=>'pages#signUp'
   get 'chat'=>'pages#chat'
+  get "/auth/facebook/callback", to: "omniauth_callbacks#facebook"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
