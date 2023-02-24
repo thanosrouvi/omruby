d46eea38780b3e46e886beaf5aa3e0485718029f
thanosrouvi <thanosrouv@gmail.com>
2023-02-24 02:02:50 +0200

    friends

---
+++ app/views/friends/_friend.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! friend, :id, :email, :friend_of, :created_at, :updated_at
+json.url friend_url(friend, format: :json)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_23_221934) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_23_235400) do
   create_table "comments", force: :cascade do |t|
     t.text "body"
     t.integer "post_id"
@@ -29,6 +29,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_23_221934) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
+  end
+
+  create_table "friends", force: :cascade do |t|
+    t.string "email"
+    t.string "friend_of"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "posts", force: :cascade do |t|
